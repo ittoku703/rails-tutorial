4bd7aee68b210f872a22235a5c3b4e54a3daf1da
shinzanmono <shinzanmono1192@gmail.com>
2021-07-01 02:15:33 +0900

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless @user.activated?
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -54,15 +54,6 @@ class UsersController < ApplicationController
     end
 
     # before_action
-
-    # check if you are logged in user
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in"
-        redirect_to login_url
-      end
-    end
 
     # check if you are correct user
     def correct_user
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# define paginate previous_label and next_label
+WillPaginate::ViewHelpers.pagination_options[:previous_label] = '&laquo'
+WillPaginate::ViewHelpers.pagination_options[:next_label]     = '&raquo'
+
